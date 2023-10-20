a38fc5de8844581050984437d6d12e63161eeba9
Abdullah Mokhtar <Abdullahmokhtr55@gmail.com>
2023-10-20 15:02:02 +0300

    responsive category

--- src/Components/CategorySlider/CategorySlider.jsx
+++ src/Components/CategorySlider/CategorySlider.jsx
@@ -12,15 +12,40 @@ const CategorySlider = () => {
   const settings = {
     infinite: true,
     speed: 500,
-    slidesToShow: 7,
+    slidesToShow: 6,
     slidesToScroll: 1,
     arrows: false,
     autoplay: true,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+    ],
   };
   return (
     <>
       <h3 className="fw-bold my-2">Shop Popular Categories</h3>
-      <Slider {...settings} className="mb-4">
+      <Slider  {...settings} className="mb-4">
         {data?.map((category, index) => (
           <React.Fragment key={index}>
             <img
